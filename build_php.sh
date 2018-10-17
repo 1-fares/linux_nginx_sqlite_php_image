ccb76d3495dbd8a424c5761f970da1fd2ab480fd
Fares <klate@admin.casagrossmann.ch>
2018-10-17 15:27:19 +0000

    more install script updates

--- build_php.sh
+++ build_php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd ../php-src && \
-./buildconf && ./configure && make && make install && \
+./buildconf && ./configure --with-mysqli --enable-fpm --with-openssl --enable-exif --enable-intl --enable-mbstring --with-zlib --enable-pcntl --with-curl --with-pdo-mysql --enable-zip && make && make install && \
 popd && \
 rm -rf ../php-src && \
 exit 0
